e9d047b96746368ca206360e48cd74b9cfb99006
Ranya <113499706+ranyabrkumar@users.noreply.github.com>
2025-11-13 20:56:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const promClient = require('prom-client');
 const { collectDefaultMetrics, Counter, Histogram, register } = require('prom-client');
 
 const app = express();
